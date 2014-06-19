372544c522a4bc2e65b888d7595dd95086312c5c
Komal Jain <komaljain1900@gmail.com>
2014-06-19 17:52:29 +0530

    Packaging Cupcakes Solution

---
+++ PackagingCupcakes.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main (String[] args) throws IOException
+	{
+	      int i, j, t, n, a=0, max=0;
+	     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	     
+	      t=Integer.parseInt(br.readLine());
+	      
+	      for(i=0; i<t; i++){
+	    
+	        n=Integer.parseInt(br.readLine());
+	     
+	       	System.out.println(n/2+1);
+	      	
+	      }
+	}
+}
